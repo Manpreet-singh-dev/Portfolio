554e253ffc9050084326561aa234ea08ea48525e
Manpreet singh <manpreetsingh53535@gmail.com>
2025-12-13 22:29:37 +0530

    path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/Github-portfolio/" : "/",
+  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
